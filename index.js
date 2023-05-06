d0de64394a968a0b1b66be9d7aedfe34ce10ff62
iimAtomic <vegbahermerite@gmail.com>
2023-05-06 17:53:29 +0200

    first and last commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const container = document.getElementById('container');
+const image = document.getElementById('image');
+const input = document.getElementById('text');
+const generateButton = container.getElementById('button1');
+const downloadButton = container.getElementById('button2');
+
+generateButton.addEventListener('click', () => {
+  const url = input.value;
+  const apiUrl = `https://api.qrserver.com/v1/create-qr-code/?size=256x256&data=
+  ${encodeURIComponent(url)}`;
+  fetch(apiUrl)
+    .then(response => response.blob())
+    .then(blob => {
+      const imageUrl = URL.createObjectURL(blob);
+      image.src = imageUrl;
+    });
+});
+downloadButton.addEventListener('click', () => {
+  const link = document.createElement('a');
+  link.download = 'Atomic-QR.png';
+  link.href = image.src;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
