6fa697dcbbe9bfc0b9956853ad00fb0192f8ed2c
iimAtomic <vegbahermerite@gmail.com>
2023-08-21 20:29:06 +0200

    perf : add  scanner

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # QR-CODE-GENERATOR
+
+It is simply a qr code generator combined with a QR code reader. The front is in html css and the backend in Javascript with instacan libraries
--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const image = document.getElementById('image');
 const input = document.getElementById('text');
 const generateButton = container.querySelector('button:nth-of-type(1)');
 const downloadButton = container.querySelector('button:nth-of-type(2)');
+const scanbutton = document.getElementById('button3');
 
 generateButton.addEventListener('click', () => {
-  const url = input.value;
+  const url = input.value ;
   const apiUrl = `https://api.qrserver.com/v1/create-qr-code/?size=256x256&data=
   ${encodeURIComponent(url)}`;
   fetch(apiUrl)
@@ -14,12 +15,53 @@ generateButton.addEventListener('click', () => {
       const imageUrl = URL.createObjectURL(blob);
       image.src = imageUrl;
     });
+    
 });
+
 downloadButton.addEventListener('click', () => {
   const link = document.createElement('a');
-  link.download = 'Atomic-QR.png';
+  link.download = input.value+'_Meet_QR.png';
   link.href = image.src;
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
+});
+
+
+scanbutton.addEventListener('click', () => {
+  document.getElementById('video').style.display = 'block';
+
+  const codeReader = new ZXing.BrowserQRCodeReader();
+
+  codeReader.getVideoInputDevices()
+    .then(videoInputDevices => {
+      // Chercher le périphérique de la caméra arrière
+      let rearCameraDevice = null;
+      for (const device of videoInputDevices) {
+        if (device.label.toLowerCase().includes('arrière') || device.label.toLowerCase().includes('rear')) {
+          rearCameraDevice = device;
+          break;
+        }
+      }
+
+      if (rearCameraDevice) {
+        codeReader.decodeFromVideoDevice(rearCameraDevice.deviceId, 'video', (result, error) => {
+          // ...
+        });
+      } else {
+        // Utiliser la première caméra disponible
+        if (videoInputDevices.length > 0) {
+          codeReader.decodeFromVideoDevice(videoInputDevices[0].deviceId, 'video', (result, error) => {
+            // ...
+          });
+        } else {
+          console.error('Aucune caméra disponible.');
+          document.getElementById('result').textContent = 'Aucune caméra disponible.';
+        }
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      document.getElementById('result').textContent = 'Erreur lors de l\'obtention des périphériques vidéo.';
+    });
 });
